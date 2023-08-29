189defbd25f304fad2b4471edda9dd79f8c95f11
Maxim Saplin <smaxmail@gmail.com>
2023-08-29 15:53:15 +0300

    More

--- build-grpc-bindings.sh
+++ build-grpc-bindings.sh
@@ -27,7 +27,11 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Prepare Pyhton dependencies
-pip3 install -r requirements.txt
+# pip3 install -r requirements.txt
+pip3 install grpcio
+pip3 install grpcio-tools
+pip3 install tinyaes
+pip3 install pyinstaller
 
 # Prepare Dart/Flutter
 brew install protobuf
@@ -35,14 +39,14 @@ dart pub global activate protoc_plugin
 export PATH="$PATH":"$HOME/.pub-cache/bin"
 
 # Generate Dart code
-mkdir -p ./$flutterDir/lib/grpc_generated/
+mkdir -p ./$flutterDir/lib/grpc_generated
 protoc --dart_out=grpc:./$flutterDir/lib/grpc_generated $proto
 cd $flutterDir
 flutter pub add grpc
 cd ..
 
 # Generate Python code
-mkdir -p ./$pythonDir/
-python3 -m grpc_tools.protoc -I. --python_out=./$pythonDir --grpc_python_out=./$pythonDir $proto
+mkdir -p ./$pythonDir/grpc_generated
+python3 -m grpc_tools.protoc -I. --python_out=./$pythonDir/grpc_generated --grpc_python_out=./$pythonDir/grpc_generated $proto
 
 echo -e "\e[32m\nDart/Flutter and Python bindings have been generated for '$proto' definition"
---
+++ service/math_operations.py
@@ -0,0 +1,11 @@
+from concurrent import futures
+import numpy as np
+
+from .grpc_generated import service_pb2_grpc
+from .grpc_generated import service_pb2
+
+class MathOperations(service_pb2_grpc.MathOperationsServicer):
+    def MatrixMultiply(self, request, context):
+        arr = np.array(request.values)
+        result = np.sort(arr)
+        return service_pb2.NumberArray(values=result)
